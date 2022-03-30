337a870e9ff4a7378245a5c0fa8277f8979eb6c9
Shibi Ramachandran <shibi.ramachandran@teampicnic.com>
2022-03-30 08:19:57 +0200

    Inject url

--- src/main/java/com/picnic/training/springproperties/A.java
+++ src/main/java/com/picnic/training/springproperties/A.java
@@ -4,24 +4,30 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.net.URI;
+import java.net.URL;
 
 @Component
 public class A {
 
-    @Value("hello.prefix")
+    @Value("${hello.prefix}")
     private String prefix;
 
-    @Value("hello.suffix")
+    @Value("${hello.suffix}")
     private String suffix;
 
-    @Value("hello.greeting")
+    @Value("${hello.greeting}")
     private String greeting;
+
+    @Value("${hello.url}")
+    private URL url;
 
     @PostConstruct
     private void init() {
         if(greeting == null || greeting.isBlank()) {
             System.out.println("Warn: greeting should not be empty");
         }
+        System.out.println(url.getHost());
     }
 
 }
